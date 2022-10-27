c8ec25a3335d4499c09eddb99176cce44dd3129e
bseright <britainseright@gmail.com>
2022-10-26 20:57:54 -0500

    animate x and y knobs on scroll up or down

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Initial state
+// Initial state xKnob
 let directionX = "";
 let secretDivX = document.querySelector(".secretDivX");
 let scrollItemX = document.querySelector(".scrollItemX");
@@ -10,20 +10,19 @@ let previousTopX;
 let xKnob = document.querySelector(".xKnob");
 let xAngle = 0;
 
-// assigning hover animation to knob when div hovered
+// assigning hover animations to knobs when secretDivs are hovered
+// xKnob hover animations
+secretDivX.addEventListener('mouseover', function() {
+    xKnob.classList.toggle("largeKnobHover");
+});
 
 secretDivX.addEventListener('mouseout', function() {
     xKnob.classList.toggle("largeKnobHover");
 });
 
-secretDivX.addEventListener('mouseover', function() {
-    xKnob.classList.toggle("largeKnobHover");
-});
-
 // establishing y measurement of each scrollable element
-
-function getInitialTopX(el) {
-   let top = 0;
+function getInitialTopX() {
+    let top = 0;
     let offsetBase = getInitialTopX.offsetBase;
   
     if (!offsetBase && document.body) {
@@ -32,16 +31,16 @@ function getInitialTopX(el) {
         document.body.appendChild(offsetBase);
     }
   
-    if (el && el.ownerDocument === document && 'getBoundingClientRect' in el && offsetBase) {
-        let boundingRect = el.getBoundingClientRect();
+    if (scrollItemX && scrollItemX.ownerDocument === document && 'getBoundingClientRect' in scrollItemX && offsetBase) {
+        let boundingRect = scrollItemX.getBoundingClientRect();
         let baseRect = offsetBase.getBoundingClientRect();
         top = boundingRect.top - baseRect.top;
     }
-  
-    initialTopX = top;
+
+    return initialTopX = top;
 }
 
-function getCurrentTopX(el) {
+function getCurrentTopX() {
     let top = 0;
     let offsetBase = getCurrentTopX.offsetBase;
   
@@ -51,43 +50,53 @@ function getCurrentTopX(el) {
         document.body.appendChild(offsetBase);
     }
   
-    if (el && el.ownerDocument === document && 'getBoundingClientRect' in el && offsetBase) {
-        let boundingRect = el.getBoundingClientRect();
+    if (scrollItemX && scrollItemX.ownerDocument === document && 'getBoundingClientRect' in scrollItemX && offsetBase) {
+        let boundingRect = scrollItemX.getBoundingClientRect();
         let baseRect = offsetBase.getBoundingClientRect();
         top = boundingRect.top - baseRect.top;
     }
   
-    currentTopX = top;
+    return currentTopX = top;
 }
 
-getInitialTopX(scrollItemX);
+// run functions on page load
+getInitialTopX();
+previousTopX = getCurrentTopX();
 
 // adding scroll event
 secretDivX.addEventListener('wheel', function() {
-  
-  getCurrentTopX(scrollItemX);
-  
-  if (previousTopX === undefined) {
-    // leaving blank to avoid preemptive direction
-  } else if (previousTopX < currentTopX || previousTopX === initialTopX && currentTopX === initialTopX) {
-    console.log(previousTopX);
-    console.log(currentTopX);
-		directionX = "UP"; 
-  } else {
-    console.log(previousTopX);
-    console.log(currentTopX)
-		directionX = "DOWN";
-  }
-  
-	// saves the new position for iteration.
-	previousTopX = currentTopX;
- 
-console.log(directionX);
+
+    setTimeout(() => { // setting timeout to allow for scroll to complete before measuring top
+
+        getCurrentTopX();
+
+        if (previousTopX < currentTopX || currentTopX === initialTopX) {
+
+            console.log(previousTopX);
+            console.log(currentTopX);
+            directionX = "UP"; 
+
+        } else {
+
+            console.log(previousTopX);
+            console.log(currentTopX)
+            directionX = "DOWN";
+
+        } 
+
+        // saves the new position for iteration.
+        previousTopX = currentTopX
+
+        console.log(directionX);
+
+        checkXRotation();
+
+    }, 115); 
+
 });
 
-// starting knob rotations
-
-secretDivX.addEventListener('wheel', function() {
+// xKnob rotation animatin
+function checkXRotation() {
 
     function rotateXClock() {
         xAngle += 30;
@@ -107,56 +116,128 @@ secretDivX.addEventListener('wheel', function() {
         rotateXCounter();
     }
     
-});
+};
+
+
+//
+//
+// Initial state yKnob
+//
+// 
+let directionY = "";
+let secretDivY = document.querySelector(".secretDivY");
+let scrollItemY = document.querySelector(".scrollItemY");
+
+let initialTopY;
+let currentTopY;
+let previousTopY;
 
 let yKnob = document.querySelector(".yKnob");
 let yAngle = 0;
 
-yKnob.addEventListener('wheel', function() {
+// yKnob hover animations
+secretDivY.addEventListener('mouseover', function() {
+    yKnob.classList.toggle("largeKnobHover");
+});
 
-    function rotateY() {
+secretDivY.addEventListener('mouseout', function() {
+    yKnob.classList.toggle("largeKnobHover");
+});
+
+// establishing y measurement of scollItemY
+function getInitialTopY() {
+    let top = 0;
+    let offsetBase = getInitialTopY.offsetBase;
+  
+    if (!offsetBase && document.body) {
+        offsetBase = getInitialTopY.offsetBase = document.createElement('div');
+        offsetBase.style.cssText = 'position:absolute;left:0;top:0';
+        document.body.appendChild(offsetBase);
+    }
+  
+    if (scrollItemY && scrollItemY.ownerDocument === document && 'getBoundingClientRect' in scrollItemY && offsetBase) {
+        let boundingRect = scrollItemY.getBoundingClientRect();
+        let baseRect = offsetBase.getBoundingClientRect();
+        top = boundingRect.top - baseRect.top;
+    }
+
+    return initialTopY = top;
+}
+
+function getCurrentTopY() {
+    let top = 0;
+    let offsetBase = getCurrentTopY.offsetBase;
+  
+    if (!offsetBase && document.body) {
+        offsetBase = getCurrentTopY.offsetBase = document.createElement('div');
+        offsetBase.style.cssText = 'position:absolute;left:0;top:0';
+        document.body.appendChild(offsetBase);
+    }
+  
+    if (scrollItemY && scrollItemY.ownerDocument === document && 'getBoundingClientRect' in scrollItemY && offsetBase) {
+        let boundingRect = scrollItemY.getBoundingClientRect();
+        let baseRect = offsetBase.getBoundingClientRect();
+        top = boundingRect.top - baseRect.top;
+    }
+  
+    return currentTopY = top;
+}
+
+// run functions on page load
+getInitialTopY();
+previousTopY = getCurrentTopY();
+
+// adding scroll event
+secretDivY.addEventListener('wheel', function() {
+
+    setTimeout(() => { // setting timeout to allow for scroll to complete before measuring top
+
+        getCurrentTopY();
+
+        if (previousTopY < currentTopY || currentTopY === initialTopY) {
+
+            console.log(previousTopY);
+            console.log(currentTopY);
+            directionY = "UP"; 
+
+        } else {
+
+            console.log(previousTopY);
+            console.log(currentTopY)
+            directionY = "DOWN";
+
+        } 
+
+        // saves the new position for iteration.
+        previousTopY = currentTopY
+
+        console.log(directionY);
+
+        checkYRotation();
+
+    }, 115); 
+
+});
+
+// yKnob rotation animatin
+function checkYRotation() {
+
+    function rotateYClock() {
         yAngle += 30;
         yKnob.style.transform = `rotate(${yAngle}deg)`;
     };
 
-    rotateY();
-});
-
-let rKnob = document.querySelector(".rKnob");
-let rAngle = 0;
-
-rKnob.addEventListener('wheel', function() {
-
-    function rotateR() {
-        rAngle += 30;
-        rKnob.style.transform = `rotate(${rAngle}deg)`;
+    function rotateYCounter() {
+        yAngle -= 30;
+        yKnob.style.transform = `rotate(${yAngle}deg)`;
     };
 
-    rotateR();
-});
-
-let gKnob = document.querySelector(".gKnob");
-let gAngle = 0;
-
-gKnob.addEventListener('wheel', function() {
-
-    function rotateG() {
-        gAngle += 30;
-        gKnob.style.transform = `rotate(${gAngle}deg)`;
-    };
-
-    rotateG();
-});
-
-let bKnob = document.querySelector(".bKnob");
-let bAngle = 0;
-
-bKnob.addEventListener('wheel', function() {
-
-    function rotateB() {
-        bAngle += 30;
-        bKnob.style.transform = `rotate(${bAngle}deg)`;
-    };
-
-    rotateB();
-});
+    if (directionY === "") {
+        // leaving empty to avoid umprompted back scroll
+    } else if (directionY === "DOWN") {
+        rotateYClock();
+    } else {
+        rotateYCounter();
+    }
+    
+};
